2fe5a23265488f956bfcfcde102f519620cf1e44
sahilkhan8786 <official.khansahil8786@gmail.com>
2024-07-23 09:26:33 +0530

    updated header

--- src/components/custom/Header.jsx
+++ src/components/custom/Header.jsx
@@ -4,13 +4,16 @@ import { Link } from 'react-router-dom'
 
 export default function Header() {
     return (
-        <div className='p-3 px-5 flex justify-between items-center h-full shadow-md'>
-            <Link to='/'>
-                <img src='/logo.png' width={50} />
-            </Link>
-            <Link to={`/dashboard`}>
-                <Button>Get Started</Button>
-            </Link>
-        </div>
+        <header className=' shadow-md w-full'>
+
+            <div className='max-w-screen-2xl mx-auto p-3 px-5 flex justify-between items-center  '>
+                <Link to='/'>
+                    <img src='/logo.png' width={50} />
+                </Link>
+                <Link to={`/dashboard`}>
+                    <Button>Get Started</Button>
+                </Link>
+            </div>
+        </header>
     )
 }
